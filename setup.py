7411bb84a53fbc13d450226e947f08ea54b1ceab
k_c_sh_7B <k_c_sh_7B@protonmail.com>
2024-09-15 20:55:04 -0400

    Cleaned up repository and added gitignore

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="linux_looker",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'requests', 
+        'colorama', 
+        'netifaces'  # Add any other dependencies
+    ],
+    entry_points={
+        'console_scripts': [
+            'linux_looker=linux_monitor:main',
+        ],
+    },
+    description="A Linux diagnostic tool for networking and system analysis.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/<your-username>/network_monitor_project",
+    author="Your Name",
+    author_email="your.email@example.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
